2ae9b48169a79a7708e049b5434c5a2587d64e80
caloxifounders-max <caloxifounders@gmail.com>
2026-03-15 16:34:35 +0200

    Create bootloader.js

---
+++ system/bootloader.js
@@ -0,0 +1,11 @@
+// RopTop-Os Boot Loader
+console.log("Checking for Extreme Demon status...");
+console.log("Loading Neon Shaders...");
+
+function launchApp(appName) {
+    console.log("Launching: " + appName);
+    alert("Opening " + appName + "... Get ready to Dash!");
+}
+
+// System Init
+console.log("RopTop-Os v0.0.1 is ONLINE.");
